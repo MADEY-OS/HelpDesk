8cd3ee74e22eda87dbd03e6dc9be4650baf35b1e
Mateusz Madej <madej256@gmail.com>
2022-09-08 13:32:45 +0200

    Zagubiony kontroller
    dodanie zagubionego kontrollera

--- HelpDesk/Controllers/RequestController.cs
+++ HelpDesk/Controllers/RequestController.cs
@@ -21,4 +21,45 @@ public class RequestController : ControllerBase
         var requestsDtos = _requestService.GetAll();
         return Ok(requestsDtos);
     }
+
+    [HttpGet("{id}")] //GET - Pobranie pojedynczego requesta
+    public ActionResult<RequestDto> GetById([FromRoute] int id)
+    {
+        var request = _requestService.GetById(id);
+
+        if (request is null) return NotFound();
+
+        return Ok(request);
+    }
+
+    [HttpPost] //POST - Dodawanie requesta
+    public ActionResult Create([FromBody] CreateRequestDto dto)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        var id = _requestService.Create(dto);
+
+        return Created($"/api/request/{id}", null);
+    }
+
+    [HttpPut("{id}")] //PUT - Modyfikowanie requesta,  rozróżnieniem nazwa/opis.
+    public ActionResult Update([FromBody] CreateRequestDto dto, [FromRoute] int id)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        var isUpdated = _requestService.Update(id, dto);
+
+        if (!isUpdated) return NotFound();
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")] //DELETE - Usuwanie requesta.
+    public ActionResult Delete([FromRoute] int id)
+    {
+        var isDeleted = _requestService.Delete(id);
+
+        if (isDeleted) return NotFound();
+
+        return NoContent();
+    }
 }
