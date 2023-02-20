2697bd0f0ba32eac69a7fe6c97c4a2c0f8b710e1
Mateusz Madej <59778797+MADEY-OS@users.noreply.github.com>
2023-02-20 05:21:01 +0100

    Update Program.cs

--- HelpDesk.Client/Program.cs
+++ HelpDesk.Client/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7217/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("") });
 builder.Services.AddScoped<IDeviceService, DeviceService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IRequestService, RequestService>();
